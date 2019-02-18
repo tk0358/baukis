bf794bf3e8388d1ca7743aef4fefacbd8bd470d8
tk0358 <tk0358a@yahoo.co.jp>
2019-02-18 13:13:04 +0900

    7-3 403 Forbidden

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
+  rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
+
   private
   def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -10,5 +17,15 @@ class ApplicationController < ActionController::Base
     else
       'customer'
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
+  end
+  
+  def rescue500(e)
+    @exception = e
+    render 'errors/internal_server_error', status: 500
   end
 end
