433a29fb22db51d88de2ef2ec8734e9216783572
tk0358 <tk0358a@yahoo.co.jp>
2019-02-19 18:10:50 +0900

    finished chapter8 ユーザ認証（１）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_062642) do
+ActiveRecord::Schema.define(version: 2019_02_19_081009) do
+
+  create_table "administrators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "email_for_index", null: false
+    t.string "hashed_password"
+    t.boolean "suspended", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_for_index"], name: "index_administrators_on_email_for_index", unique: true
+  end
 
   create_table "staff_members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
