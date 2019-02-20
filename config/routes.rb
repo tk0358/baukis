6741d6eb37fcd8c2bf38cb81b7dc224e5307f8ee
tk0358 <tk0358a@yahoo.co.jp>
2019-02-20 13:51:08 +0900

    finished9-2(FactoryBot失敗し、コメントアウト)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'session#destroy'
+    delete 'session' => 'sessions#destroy'
   end
 
   namespace :admin do
